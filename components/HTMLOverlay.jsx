2f8ec4b921c96c10e798daa226d57d6fa8c5da06
Orsbert Ayesigye <45017033+Orsbert@users.noreply.github.com>
2024-03-27 12:30:24 +0300

    Add Stats component to HTMLOverlay

--- components/HTMLOverlay.jsx
+++ components/HTMLOverlay.jsx
@@ -1,20 +1,24 @@
+import { Stats } from "@react-three/drei";
 import React from "react";
 
 const styles = {
-  position: 'fixed',
+  position: "fixed",
   top: 0,
-  height: '100vh',
-  width: '100vw',
-  pointerEvents: 'none',
+  height: "100vh",
+  width: "100vw",
+  pointerEvents: "none",
 };
 
 export const HTMLOverlay = () => {
   return (
     <>
       {/* @ts-ignore */}
-      <div className="text-green-500 fixed top-0 left-0 h-full w-full pointer-events-none" style={styles}>
-        <h1>Helloo...</h1>
+      <div
+        className='text-green-500 fixed top-0 left-0 h-full w-full pointer-events-none'
+        style={styles}
+      >
+        <Stats />
       </div>
     </>
   );
-}
+};
